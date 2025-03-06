1dfb3c37d925a52ffa3f23cc90ee7bb5addff07b
Aklile Ansa <aklilemente72@gmail.com>
2025-03-07 01:16:07 +0300

    Rows and RowList

--- Netflix Clone/src/Pages/Home/Home.jsx
+++ Netflix Clone/src/Pages/Home/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import Banner from '../../Components/Banner/Banner';
+import Row from '../../Components/rows/Row/Row';
+import RowList from '../../Components/rows/RowList/RowList';
 
 function Home() {
   return (
     <>
       <Header/>
       <Banner/>
+      <RowList/>
       <Footer/>
     </>
   )
