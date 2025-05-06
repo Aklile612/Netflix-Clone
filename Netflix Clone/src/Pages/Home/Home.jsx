87561670b5fd0aeb7cf6550c84e8e4e96e2c5ff7
Aklile612 <aklilemente72@gmail.com>
2025-05-06 10:00:00 +0300

    Remove unused imports

--- Netflix Clone/src/Pages/Home/Home.jsx
+++ Netflix Clone/src/Pages/Home/Home.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import Banner from '../../Components/Banner/Banner';
-import Row from '../../Components/rows/Row/Row';
 import RowList from '../../Components/rows/RowList/RowList';
 
 function Home() {
