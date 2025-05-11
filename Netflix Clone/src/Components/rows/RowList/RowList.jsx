99b5aa95c3acfeb83787c95bd2052b6ae07f8493
Aklile612 <aklilemente72@gmail.com>
2025-05-11 10:00:00 +0300

    Only use large posters for Netflix Originals row

--- Netflix Clone/src/Components/rows/RowList/RowList.jsx
+++ Netflix Clone/src/Components/rows/RowList/RowList.jsx
@@ -12,42 +12,34 @@ const RowList = () => {
     <Row 
         title = "Trending Now"
         fetchUrl={requests.fetchTrending}
-        isLargeRow={true}
     />
     <Row 
         title = "Top Rated"
         fetchUrl={requests.fetchTopRatedMovies}
-        isLargeRow={true}
     />
     <Row 
         title = "Action Movies"
         fetchUrl={requests.fetchActionMovies}
-        isLargeRow={true}
     />
     <Row 
         title = "Comedy Movies"
         fetchUrl={requests.fetchComedyMovies}
-        isLargeRow={true}
     />
     <Row 
         title = "Horror Movies"
         fetchUrl={requests.fetchHorrorMovies}
-        isLargeRow={true}
     />
     <Row 
         title = "Romance Movies"
         fetchUrl={requests.fetchRomanceMovies}
-        isLargeRow={true}
     />
     <Row 
         title = "TV Shows"
         fetchUrl={requests.fetchTvShow}
-        isLargeRow={true}
     />
     <Row 
         title = "Documentaries"
         fetchUrl={requests.fetchDocumentaries}
-        isLargeRow={true}
     />
     </>
   )
